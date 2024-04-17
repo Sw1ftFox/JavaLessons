8178e8babb02a93d7d18407c0d34d82a280433cd
Sw1ftFox <v.suvorkin2017@gmail.com>
2024-04-17 22:47:21 +0300

    added variables and strings

---
+++ src/Strings.java
@@ -0,0 +1,11 @@
+public class Strings {
+    public static void main(String[] args) {
+        int x = 5;
+        String s = "Hello";//является классом и ссылкой. класс что-то вроде чертежа, а объект - уже созданный по нему предмет
+        String space = " ";
+        String name = "Bob";
+        System.out.println(s + space + name);
+        System.out.println("Hello" + " " + "John");
+        System.out.println("My number is: " + x);//shortkup "sout"
+    }
+}
---
+++ src/Variables.java
@@ -0,0 +1,16 @@
+public class Variables {
+    public static void main(String[] args) {
+        int myInt = 557;
+        short myShort = 3266; //от -32_000 до 32_000
+        long myLong = 2562375;
+
+        double myDouble = 235.35;//число с точкой всегда по умолчанию double
+        float myFloat =2362.4f;//если поставить в конце f, то компилятор поймет тип данных как float
+
+        char c = 'a';//символ, буква
+        boolean b = true; //false
+
+        byte myByte = 100;//-128 до 128
+        System.out.println(myInt);//вывод
+    }
+}
